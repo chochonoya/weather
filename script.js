7b21871a5104ef19835f67f602513958fb31799f
chochonoya <adalilovna1@gmail.com>
2023-06-12 16:54:42 +0600

    full finish

--- script.js
+++ script.js
@@ -3,21 +3,30 @@ let appID = "&appid=b067377a72c98ae6963cdae2e35408d9";
 
 let input = document.getElementById("input");
 
-async function getWindyByCity(event) {
+async function getWeatherByCity(event) {
   event.preventDefault();
   const response = await fetch(API + input.value + appID);
   const weather = await response.json();
   console.log("weather: ", weather);
-  const fills_like = Math.floor(weather.main.feels_like - 273.15);
-  const temp = Math.floor(weather.main.temp - 273.15);
+  const feels_like = Math.floor(weather.main.feels_like - 273.15);
 
-  let resultBlcok = document.getElementById("resultBlock");
+  let resultBlock = document.getElementById("resultBlock");
+  resultBlock.style.height = "auto";
+  resultBlock.innerHTML = "";
 
-  let fills_likenode = document.createElement("span");
-  fills_likenode.innerHTML = `feels like in ${input.value} : ${fills_like}`;
-  resultBlcok.appendChild(fills_likenode);
+  let feels_likeNode = document.createElement("span");
+  feels_likeNode.innerHTML = `Feels like in ${input.value}: ${feels_like}`;
+  resultBlock.appendChild(feels_likeNode);
 
-  let tempnode = document.createElement("span");
-  tempnode.innerHTML = ` temp ${input.value} : ${temp}`;
-  resultBlcok.appendChild(tempnode);
+  let tempNode = document.createElement("span");
+  tempNode.innerHTML = `Temperature in ${input.value}: ${feels_like}`;
+  resultBlock.appendChild(tempNode);
+
+  let countryNode = document.createElement("span");
+  countryNode.innerHTML = `Country: ${weather.sys.country}`;
+  resultBlock.appendChild(countryNode);
+}
+
+function clearInput() {
+  input.value = "";
 }
